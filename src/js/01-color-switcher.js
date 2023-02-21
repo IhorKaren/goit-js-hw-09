757d8e743a57521dd01ada2ae972ec73f0d40f6b
Ihor Karen <karenihor@gmail.com>
2023-02-21 14:35:59 +0200

    timer v1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,27 @@
 const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('[data-stop]')
+const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
 
-startBtn.addEventListener('click', onStartBtnClick)
-stopBtn.addEventListener('click', onStopBtnClick)
-stopBtn.setAttribute("disabled", "disabled")
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+stopBtn.setAttribute('disabled', 'disabled');
 
 function onStartBtnClick() {
-    timerId = setInterval(() => {
-        document.body.style.backgroundColor = getRandomHexColor()
-    }, 1000)
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 
-    startBtn.setAttribute("disabled", "disabled")
-    stopBtn.removeAttribute("disabled")
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
 }
 
 function onStopBtnClick() {
-    clearInterval(timerId)
-    
-    stopBtn.setAttribute("disabled", "disabled")
-    startBtn.removeAttribute("disabled")   
+  clearInterval(timerId);
+
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled');
 }
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,97 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  inputDate: document.querySelector('[id="datetime-picker"]'),
+  startBtn: document.querySelector('[data-start]'),
+  timerDays: document.querySelector('[data-days]'),
+  timerHours: document.querySelector('[data-hours]'),
+  timerMins: document.querySelector('[data-minutes]'),
+  timerSec: document.querySelector('[data-seconds]'),
+  dateValue: null,
+};
+
+refs.startBtn.addEventListener('click', onStartTimer);
+
+const timer = {
+  isActive: false,
+
+  start(value) {
+    if (this.isActive) {
+      return;
+    }
+
+    const startTime = value;
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = convertMs(deltaTime);
+
+      onTimerSetValue(time);
+    }, 1000);
+  },
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    onDateCheck(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.inputDate, options);
+onDisableBtn();
+
+function onDateCheck(value) {
+  if (value.getTime() < Date.now()) {
+    window.alert('Please choose a date in the future');
+    onDisableBtn();
+    return;
+  }
+
+  refs.startBtn.removeAttribute('disabled');
+  return (refs.dateValue = value.getTime());
+}
+
+function onTimerSetValue({ days, hours, minutes, seconds }) {
+  refs.timerDays.textContent = addLeadingZero(`${days}`.replace('-', '') - 1);
+  refs.timerHours.textContent = addLeadingZero(`${hours}`.replace('-', '') - 1);
+  refs.timerMins.textContent = addLeadingZero(`${minutes}`.replace('-', '') - 1);
+  refs.timerSec.textContent = addLeadingZero(`${seconds}`.replace('-', ''));
+}
+
+function onStartTimer() {
+  timer.start(refs.dateValue);
+  timer.isActive = true;
+}
+
+function onDisableBtn() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart('2', '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
